b11d6685925fad8784d4e17483fb560e8db2eaaa
manticorevault <artur_seilah@hotmail.com>
2021-01-11 19:49:04 +0000

    Some basics and some tools related to C++ and the eJPT course

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Digai seus fi do cabrunco" << endl;
+}
---
+++ input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int uservalue;
+
+    cout << "This program add 10 to your input." << endl;
+    cout << "Please insert your input: ";
+
+    cin >> uservalue;
+
+    cout << "The value inserted is " << uservalue;
+    cout << " and the new value is " << uservalue + 10 << endl;
+
+    cin.ignore();
+    cout << "Press Enter to exit...";
+    cin.ignore();
+
+    return 0;
+}
